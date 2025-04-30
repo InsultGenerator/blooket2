0ee54cb02e31b2abbc9e7227a5ad5e8844025e1e
Bubble <Khaledalthahabi7@gmail.com>
2025-04-30 10:57:37 -0400

    script.js

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 function login() {
   const user = document.getElementById("username").value;
   const pass = document.getElementById("password").value;
+  const loadingDiv = document.getElementById("loading");
+  const dashboardDiv = document.getElementById("dashboard");
+  const loginSound = document.getElementById("login-sound");
 
   if (
     user === "j.stevens@blooket.com" &&
     pass === "Blu0k3t!Emp#0492"
   ) {
-    document.getElementById("dashboard").style.display = "block";
+    loadingDiv.style.display = "block";
+
+    setTimeout(() => {
+      loadingDiv.style.display = "none";
+      dashboardDiv.style.display = "block";
+      loginSound.play();
+    }, 3000); 
   } else {
     alert("Access Denied: Invalid credentials");
   }
